aa500eb7d57951f7e4ce0fbb9f7e831cec74c920
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2012-08-27 19:57:26 -0300

    Criação do primeiro projeto: Touch

---
+++ 1-Touch/Touch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Touch
+//
+//  Created by Cleverson Sacramento on 27/08/12.
+//  Copyright (c) 2012 Cleverson Sacramento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
