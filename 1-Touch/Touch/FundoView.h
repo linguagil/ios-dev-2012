dc9bfd2fb36053e85e29cf34b8b9a9c20429ec51
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2012-08-27 20:09:07 -0300

    Criação da classe FundoView

---
+++ 1-Touch/Touch/FundoView.h
@@ -0,0 +1,13 @@
+//
+//  FundoView.h
+//  Touch
+//
+//  Created by Cleverson Sacramento on 27/08/12.
+//  Copyright (c) 2012 Cleverson Sacramento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FundoView : UIView
+
+@end
---
+++ 1-Touch/Touch/FundoView.m
@@ -0,0 +1,13 @@
+//
+//  FundoView.m
+//  Touch
+//
+//  Created by Cleverson Sacramento on 27/08/12.
+//  Copyright (c) 2012 Cleverson Sacramento. All rights reserved.
+//
+
+#import "FundoView.h"
+
+@implementation FundoView
+
+@end
