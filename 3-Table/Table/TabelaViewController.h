7421e01c7798a5ea053a16713babd8182c2b8cb4
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2012-08-28 11:53:41 -0300

    Criação do TableViewController customizado

---
+++ 3-Table/Table/TabelaViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabelaViewController.h
+//  Table
+//
+//  Created by Cleverson Sacramento on 28/08/12.
+//  Copyright (c) 2012 Cleverson Sacramento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabelaViewController : UITableViewController
+
+@end
