171bc8cccd993a585be9479afaf6eaa6cac94fc4
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2012-08-28 11:18:19 -0300

    Criação do terceiro projeto para experimentar UITableViewController

---
+++ 3-Table/Table/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Table
+//
+//  Created by Cleverson Sacramento on 28/08/12.
+//  Copyright (c) 2012 Cleverson Sacramento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 3-Table/Table/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Table
+//
+//  Created by Cleverson Sacramento on 28/08/12.
+//  Copyright (c) 2012 Cleverson Sacramento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
