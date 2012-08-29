3c1538eda2dfe4d700addf3e4ddee8a2616b712f
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2012-08-29 14:19:12 -0300

    Persistindo os dados com NSUserDefaults
    Observe que, graças a arquitetura bem definida, só foi necessário
    modificar uma classe!

--- 3-Table/Table/ItemManager.m
+++ 3-Table/Table/ItemManager.m
@@ -8,47 +8,43 @@
 
 #import "ItemManager.h"
 
-@interface ItemManager ()
-
-+ (NSMutableArray *)itens;
-
-@end
-
 @implementation ItemManager
-
-+ (NSMutableArray *)itens
-{
-    static NSMutableArray *_itens;
-    
-    if(!_itens) {
-        _itens = [[NSMutableArray alloc] init];
-        
-        Item *item;
-        
-        item = [[Item alloc] init];
-        item.nome = @"LinguÁgil 2012";
-        [_itens addObject:item];
-        
-        item = [[Item alloc] init];
-        item.nome = @"iOS Dev Bahia";
-        [_itens addObject:item];
-        
-        item = [[Item alloc] init];
-        item.nome = @"UCSal";
-        [_itens addObject:item];
-    }
-    
-    return _itens;
-}
 
 + (NSArray *)obterTodos
 {
-    return [self itens];
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSArray *dictionaries = [userDefaults objectForKey:@"itens"];
+
+    Item *item;
+
+    for(NSDictionary *dictionary in dictionaries) {
+        item = [[Item alloc] init];
+        [item setValuesForKeysWithDictionary:dictionary];
+
+        [result addObject:item];
+    }
+    
+    return result;
 }
 
 + (void)persistirItem:(Item *)item
 {
-    [[self itens] addObject:item];
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSMutableArray *dictionaries = [userDefaults objectForKey:@"itens"];
+
+    if (!dictionaries){
+        dictionaries = [[NSMutableArray alloc] init];
+    } else {
+        dictionaries = [dictionaries mutableCopy];
+    }
+    
+    NSDictionary *dictionary = [item dictionaryWithValuesForKeys:@[@"nome"]];
+    [dictionaries addObject:dictionary];
+    
+    [userDefaults setValue:dictionaries forKey:@"itens"];
+    [userDefaults synchronize];
 }
 
 @end
