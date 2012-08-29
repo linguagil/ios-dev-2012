ed30dd91e3e8367986fd88a83c0665b5e27e271e
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2012-08-29 09:51:58 -0300

    Criação da entidade Item

---
+++ 3-Table/Table/Item.h
@@ -0,0 +1,15 @@
+//
+//  Item.h
+//  Table
+//
+//  Created by Cleverson Sacramento on 29/08/12.
+//  Copyright (c) 2012 Cleverson Sacramento. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Item : NSObject
+
+@property NSString *nome;
+
+@end
---
+++ 3-Table/Table/Item.m
@@ -0,0 +1,15 @@
+//
+//  Item.m
+//  Table
+//
+//  Created by Cleverson Sacramento on 29/08/12.
+//  Copyright (c) 2012 Cleverson Sacramento. All rights reserved.
+//
+
+#import "Item.h"
+
+@implementation Item
+
+@synthesize nome;
+
+@end
